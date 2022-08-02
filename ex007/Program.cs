9af0ad4760297385ac06504d016699134c6172a6
zvnic <zvnman@yandex.ru>
2022-08-02 20:29:46 +0300

    ex007 фрактал Серпинского

---
+++ ex007/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.Clear();
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+int x = xa, y = ya;
+
+int count = 0;
+
+while(count < 100000)
+{
+    int what = new Random().Next(0,3);
+
+    if(what == 0) {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+}
+
