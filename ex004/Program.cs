eb298e9808b15377c5a665b930ca589b750f210a
zvnic <zvnman@yandex.ru>
2022-08-02 19:47:21 +0300

    ex004 random

--- ex004/Program.cs
+++ ex004/Program.cs
@@ -1,4 +1,4 @@
-﻿double numberA = 12;
-double numberB = 5;
-
-Console.WriteLine(numberA/numberB);
+﻿double numberA = new Random().Next(1,10);
+double numberB = new Random().Next(1,10);
+double result = numberA/numberB;
+Console.WriteLine(result);
