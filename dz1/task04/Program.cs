1029b74da78931a8b9537bcfefd631ab44660f76
zvnic <zvnman@yandex.ru>
2022-08-04 23:00:14 +0300

    dz task-04 done

--- dz1/task04/Program.cs
+++ dz1/task04/Program.cs
@@ -4,5 +4,22 @@
     {
         Console.Clear();
         Console.WriteLine("#Task-04");
+
+        int max = 0;
+
+        Console.WriteLine("Введите первое целое число:");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите второе целое число:");
+        int numberB = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите третье целое число:");
+        int numberС = Convert.ToInt32(Console.ReadLine());
+
+        if(numberA > max) max = numberA;
+        if(numberB > max) max = numberB;
+        if(numberС > max) max = numberС;
+
+        Console.WriteLine($"Из чисел({numberA},{numberB},{numberС}) max = {max}");
     }
 }
