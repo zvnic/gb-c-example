9ed374183c5f9c862619ed3fb7b0874898b22e1b
zvnic <zvnman@yandex.ru>
2022-08-04 23:14:30 +0300

    dz task-08 done

--- dz1/task08/Program.cs
+++ dz1/task08/Program.cs
@@ -4,5 +4,14 @@
     {
         Console.Clear();
         Console.WriteLine("#Task-08");
+
+        Console.WriteLine("Введите целое число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"Чётные числа [от 1 до {number}] - ");
+
+        for(int i = 1; i <= number; i++){
+            if(i%2 == 0) Console.Write($" {i} ");
+        }
     }
 }
