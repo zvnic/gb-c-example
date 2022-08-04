19a702d150c2520135f1cdfff2d900a9bf65f9ea
zvnic <zvnman@yandex.ru>
2022-08-04 21:29:15 +0300

    add seminar1 task3

---
+++ seminar1/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Введите номер дня:");
+        int numberDay = Convert.ToInt32(Console.ReadLine());
+
+        if(numberDay == 1) Console.WriteLine("Понедельник");
+        else if(numberDay == 2) Console.WriteLine("Вторник");
+        if(numberDay == 3) Console.WriteLine("Среда");
+        if(numberDay == 4) Console.WriteLine("Четверг");
+        if(numberDay == 5) Console.WriteLine("Пятница");
+        if(numberDay == 6) Console.WriteLine("Суббота");
+        if(numberDay == 7) Console.WriteLine("Воскресенье");
+
+        if(numberDay <= 0 || numberDay > 7) Console.WriteLine("Нет такого дня недели!");
+
+    }
+}
---
+++ seminar1/task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = -num; i <= num; i++)
+{
+    Console.WriteLine(i);
+}
