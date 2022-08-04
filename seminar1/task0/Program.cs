7c41291c7fd1fb947a197fbb4f7167ae97a97af7
zvnic <zvnman@yandex.ru>
2022-08-04 20:38:37 +0300

    add seminar1 task0

---
+++ seminar1/task0/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+**
+*/
+
+int userNumber = new int();
+
+Console.WriteLine("Введите число:");
+
+userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = (userNumber * userNumber);
+
+Console.WriteLine("Степень числа [" + result + "] = " + result);
+Console.WriteLine($"Степень числа [{userNumber}] = {result}");
