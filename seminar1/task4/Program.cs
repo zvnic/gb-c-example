893d963d0de1791e02fe0e12271ced3284d9b6a7
zvnic <zvnman@yandex.ru>
2022-08-04 21:40:37 +0300

    add seminar1 task4

---
+++ seminar1/task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num2 = num%10;
+
+Console.WriteLine(num2);
