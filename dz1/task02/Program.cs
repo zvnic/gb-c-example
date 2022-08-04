96d3da89021962cec8737ce513124f0992858adb
zvnic <zvnman@yandex.ru>
2022-08-04 22:48:05 +0300

    dz task-02 done

--- dz1/task02/Program.cs
+++ dz1/task02/Program.cs
@@ -4,5 +4,18 @@
     {
         Console.Clear();
         Console.WriteLine("#Task-02");
+        
+        Console.WriteLine("Введите первое целое число:");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите второе целое число:");
+        int numberB = Convert.ToInt32(Console.ReadLine());
+
+        if(numberA > numberB){
+            Console.WriteLine($"Из чисел ({numberA},{numberB}) большее число = {numberA}");
+        }
+        else {
+            Console.WriteLine($"Из чисел ({numberA},{numberB}) большее число = {numberB}");
+        }
     }
 }
