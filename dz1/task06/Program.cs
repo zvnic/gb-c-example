9aedccc490c4448052ea01662f93b8d8cb8d07d3
zvnic <zvnman@yandex.ru>
2022-08-04 23:07:21 +0300

    dz task-06 done

--- dz1/task06/Program.cs
+++ dz1/task06/Program.cs
@@ -4,5 +4,14 @@
     {
         Console.Clear();
         Console.WriteLine("#Task-06");
+
+        Console.WriteLine("Введите целое число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if(number%2 == 0) {
+            Console.WriteLine($"Число {number} - четное!");
+        } else {
+            Console.WriteLine($"Число {number} - не четное!");
+        }
     }
 }
