ceda5c4c2afb17bf09fa19478acb4d45ac8d36e5
zvnic <zvnman@yandex.ru>
2022-08-04 20:55:55 +0300

    add seminar1 task1

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 == number1 * number1) {
+    Console.WriteLine("true");
+} else {
+    Console.WriteLine("false");
+}
