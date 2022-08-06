e3cabf734e8ee9658f2dd31aae0a5df8b228dfab
zvnic <zvnman@yandex.ru>
2022-08-06 12:17:09 +0300

    add lecture 2

---
+++ lecture2/ex001/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("#Function");
+
+int Max(int arg1, int arg2, int arg3) {
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+Console.WriteLine("max= " + Max(100,200,300));
