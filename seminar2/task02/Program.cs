aaa81d5434d9c6fb5257eda57c24b08952f7830c
zvnic <zvnman@yandex.ru>
2022-08-08 21:40:37 +0300

    seminar2 task02

---
+++ seminar2/task02/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int GetRandomNumber() {
+    int result = new Random().Next(100, 999);
+    return result;
+}
+
+int num = GetRandomNumber();
+
+Console.WriteLine($"{num} - {num/100}");
+Console.WriteLine($"{num} - {num%100/10}");
+Console.WriteLine($"{num} - {num%10}");
