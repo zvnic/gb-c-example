cd2d9a511e6358a22e580c1e14d3d92488db4b40
zvnic <zvnman@yandex.ru>
2022-08-08 22:54:12 +0300

    dz2 task10, task13 - done

---
+++ dz2/task10/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Clear();
+Console.WriteLine("#task10");
+
+int num = new Random().Next(100, 999);
+Console.WriteLine($"{num} -> {num%100/10}");
+
+
---
+++ dz2/task13/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.WriteLine("#task113");
+
+int[] arrayNumber = {645, 78, 32679, 3478, 8123, 565, 78946, 84};
+
+int result = 0;
+
+for(int i=0; i < arrayNumber.Length; i++) {
+
+    if (arrayNumber[i] < 100) {
+        result = -1;
+    }
+
+    if(arrayNumber[i] >= 100 && arrayNumber[i] <= 999) {
+        result = arrayNumber[i]%100%10;
+    }
+
+    if(arrayNumber[i] > 999 && arrayNumber[i] <= 9999) {
+        result = arrayNumber[i]%1000%100/10;
+    }
+
+    if(arrayNumber[i] > 9999) {
+        result = arrayNumber[i]%1000/100;
+    }
+
+    if(result == -1) 
+    {
+        Console.WriteLine($"{arrayNumber[i]} -> третьей цифры нет!");
+    } else {
+        Console.WriteLine($"{arrayNumber[i]} -> {result}");
+    }
+    
+}
