2371513ca0264b5e33cfae497f0af810e57c2ac9
zvnic <zvnman@yandex.ru>
2022-08-08 21:56:07 +0300

    seminar2 task03

---
+++ seminar2/task03/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1 = new Random().Next(10, 99);
+int num2 = new Random().Next(10, 999);
+
+if(num1%num2 == 0) Console.WriteLine($"{num1} - {num2} = кратно");
+else Console.WriteLine($"{num1} - {num2} = не кратно");
