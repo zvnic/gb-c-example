a7b07a5ad8fea297597053bb7e8be5b60bf67f09
zvnic <zvnman@yandex.ru>
2022-08-11 22:20:38 +0300

    dz3 task_19 palindromeCalculation - done

---
+++ dz3/task_19/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 19.
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.Clear();
+Console.WriteLine("***** DZ_task_19 *****");
+
+Console.Write("Введите целое число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int palindromeCalculation(int num) {
+    int res = 0;
+    int d = 0;
+    while (num > 0) {
+        d = num % 10;
+        res = res * 10 + d;
+        num = num / 10; 
+    }
+    return res;
+}
+
+if(num == palindromeCalculation(num)) {
+    Console.WriteLine($"Число {num} является палиндромом!");
+} else {
+    Console.WriteLine($"Число {num} не является палиндромом!");
+}
--- seminar3/task02/Program.cs
+++ seminar3/task02/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+Console.WriteLine("#Task_22");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= n; i++) {
+    Console.Write($"{i * i}, ");
+}
