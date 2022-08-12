cdcaf7c3668f2abdccd22a3602e9914b43e01e6d
zvnic <zvnman@yandex.ru>
2022-08-12 13:08:22 +0300

    dz3 task_23 num cube - done

--- dz3/task_23/Program.cs
+++ dz3/task_23/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.Clear();
+Console.WriteLine("***** DZ_task_23 *****");
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{n} -> 1");
+
+for(int i=2; i <= n; i++) {
+    Console.Write($", {Math.Pow(i, 3)}");
+}
