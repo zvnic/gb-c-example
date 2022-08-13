8b999495d0adb597f778ea6ac61bbce1e1e7bbed
zvnic <zvnman@yandex.ru>
2022-08-13 12:50:08 +0300

    add lecture3 ex001,002,004

--- lecture3/ex003/Program.cs
+++ lecture3/ex003/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] arr = {1,2,3,4,5,6,7,7,5,56,7,8};
+
+void PrintArray(int[] array) {
+    int count = array.Length;
+
+    for(int i=0; i < count; i++) {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+
+}
+
+void SelectionSort(int[] array) {
+    for(int i=0; i <array.Length; i++) {
+        int minPosition = i;
+        
+        for(int j= i+1; j <array.Length; j++ ) {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temp = array[i];
+        array[minPosition] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
