64b1132467e6fdff269a11449c3f9d8187c28466
zvnic <zvnman@yandex.ru>
2022-08-20 12:15:13 +0300

    lecture4

---
+++ lecture4/ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[,] table =new string[10, 10];
+
+// table[0,0] table[0,1]
+table[1,2] = "test";
+
+for(int i=0; i < table.GetLength(0); i++) {
+    for(int j=0; j < table.GetLength(1); j++) {
+        table[i,j] = "" + new Random().Next(100);
+    }
+    Console.WriteLine();
+}
+
+for(int i=0; i < table.GetLength(0); i++) {
+    for(int j=0; j < table.GetLength(1); j++) {
+        Console.Write($" {table[i,j]} ");
+    }
+    Console.WriteLine();
+}
