769409e4b350b13dd9de83f79fc5c606aba774b0
zvnic <zvnman@yandex.ru>
2022-09-02 23:45:55 +0300

    add dz9 task-66

---
+++ dz9/task_64/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+ в промежутке от M до N от большего к меньшему.
+M = 1; N = 5. -> ""5, 4, 3, 2, 1""
+M = 4; N = 8. -> ""8, 7, 6, 5, 4""
+*/
+Console.Clear();
+Console.WriteLine("#Task-64");
+
+int GetAllNaturalNumbers(int m, int n) {
+    if(n < m) return 1;
+    else {
+        Console.Write($"{n} ");
+        return GetAllNaturalNumbers(m, n - 1) - n;
+    }
+}
+
+void CheckNumbers(int m, int n) {
+    if (m > n) Console.WriteLine($"Ошибка: m[{m}] > n[{n}]");
+    else {
+        GetAllNaturalNumbers(m, n);
+    }
+}
+
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+CheckNumbers(m, n);
---
+++ dz9/task_66/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму чётных чисел
+натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 56
+M = 4; N = 8. -> 18
+*/
+Console.Clear();
+Console.WriteLine("#Task-66");
+
+
+int GetSumOfPrivateNumbers(int m, int n) {
+    if (m <= n) {
+        if (m % 2 == 0) {
+            return m + GetSumOfPrivateNumbers(m + 2, n);
+        } else {
+            m = m + 1;
+            return m + GetSumOfPrivateNumbers(m + 2, n);
+        }
+    }
+    else return 0;
+
+}
+
+void CheckNumbers(int m, int n) {
+    if (m > n) Console.WriteLine($"Ошибка: m[{m}] > n[{n}]");
+    else {
+        Console.WriteLine($"Сумма чётных чисел натуральных элементов в промежутке от {m} до {n} = " + GetSumOfPrivateNumbers(m, n));
+    }
+}
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+CheckNumbers(m, n);
